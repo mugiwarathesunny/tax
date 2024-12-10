80b3ecb48f9e5132d0e6f764d13678139e1e2f20
Toluwalase Cephas Akindolie <000870019@mohawkcollege.ca>
2024-12-10 03:59:30 -0500

    commit

--- src/main/java/com/dufana/berlin/toluakindolie/tax/controller/TaxController.java
+++ src/main/java/com/dufana/berlin/toluakindolie/tax/controller/TaxController.java
@@ -1,22 +1,41 @@
 package com.dufana.berlin.toluakindolie.tax.controller;
 
+import com.dufana.berlin.toluakindolie.tax.entity.TaxBracket;
+import com.dufana.berlin.toluakindolie.tax.repository.TaxBracketRepository;
 import com.dufana.berlin.toluakindolie.tax.service.TaxCalculationService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/tax")
+@RequestMapping("/api/v1/tax")
 public class TaxController {
     private final TaxCalculationService taxCalculationService;
+    private final TaxBracketRepository taxBracketRepository;
 
-    public TaxController(TaxCalculationService taxCalculationService) {
+    public TaxController(TaxCalculationService taxCalculationService, TaxBracketRepository taxBracketRepository) {
         this.taxCalculationService = taxCalculationService;
+        this.taxBracketRepository = taxBracketRepository;
     }
 
     @GetMapping("/calculate")
-    public double caculateTax(@RequestParam double income){
-        return taxCalculationService.calculateTax(income);
+    public double calculateTax(@RequestParam double income) {
+        List<TaxBracket> taxBrackets = taxBracketRepository.findAll();
+        double tax = 0;
+        for (TaxBracket bracket : taxBrackets) {
+            if (income > bracket.getLowerBound()) {
+                double taxableIncome = Math.min(income, bracket.getUpperBound() - bracket.getLowerBound());
+                tax += taxableIncome * bracket.getRate();
+                System.out.println("Bracket: " + bracket);
+                System.out.println("Taxable Income: " + taxableIncome);
+                System.out.println("Bracket Tax: " + tax);
+            }
+        }return tax;
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleIllegalArgumentException(IllegalArgumentException e) {
+        return e.getMessage();
     }
 }
